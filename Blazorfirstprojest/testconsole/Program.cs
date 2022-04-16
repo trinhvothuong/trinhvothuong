6f12559f52d2796af41bd1b58688e2341786ba7e
vothuong <trinhbrian65@gmail.com>
2022-04-16 16:35:34 -0700

    later

--- Blazorfirstprojest/testconsole/Program.cs
+++ Blazorfirstprojest/testconsole/Program.cs
@@ -5,45 +5,9 @@ class TestClass
 
     static void Main(string[] data)
     {
-
-        //string FirstNumber = null;
-        //string FirstNumber = null;
-        //Console.WriteLine("Enter your firstnumber:" );
-
-        //FirstNumber =    Console.ReadLine();
-
-        //Console.WriteLine("Enter your secondnumber:");
-
-        //SecondNumber = Console.ReadLine();
-
-        //Console.Write("Your two number is: ");
-        //Console.Write(FirstNumber + " " + SecondNumber );
-
-        //// Console.Read();
-
-        int FirstNumber = 0;
-        int SecondNumber = 0;
-        int TotalNumber = 0;
-        string temp = null;
-
-        Console.WriteLine("Enter your firstnumber:");
-        temp = Console.ReadLine();
-        FirstNumber = int.Parse(temp);
-
-        Console.WriteLine("Enter your secondnumber:");
-        temp = Console.ReadLine();
-        SecondNumber = int.Parse(temp);
-
-        Console.Write("Your two number enter is: ");
-        Console.WriteLine(FirstNumber + " " + SecondNumber);
-
-
-        TotalNumber = FirstNumber + SecondNumber;
-        Console.Write("Your total is: ");
-        Console.Write(TotalNumber);
-
-
-
+        Console.WriteLine("hello cites");
+        HttpClient caller = new HttpClient();
+        caller.GetAsync("https://localhost:7236/weatherforecast").GetAwaiter
 
         Console.Read();
 
